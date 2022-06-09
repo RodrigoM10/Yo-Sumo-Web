705ebb96e1506829e773bebd769c21bd599f419e
Josesan94 <josesantillan2@hotmai>
2022-06-08 23:36:22 -0300

    Old Form Eliminated

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {  Route, Routes } from 'react-router-dom';
 import PublicLayout from '../layout/PublicLayout';
 import Home from '../pages/Home';
-import Form from '../components/Form/Form';
 import PetitionForm from '../components/MultistepForm/index';
 import Summary from '../components/MultistepForm/Summary/Summary';
 
